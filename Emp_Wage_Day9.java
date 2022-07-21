00e5e113411caeff14ae9033073fa940361ec420
vishal5394 <vishaljadyal@gmail.com>
2022-07-22 00:10:19 +0530

    Added: Attendance Of Employee Wage.

--- Emp_Wage_Day9.java
+++ Emp_Wage_Day9.java
@@ -5,9 +5,21 @@ package com.BridgeLabz.day9;
 		void  Welcome(){
 			System.out.println("Welcome to Employee Wage Computation Program");
 		}
+		void Attendance() {
+			int Is_Present = 1;
+			double Attendance = Math.floor(Math.random()*10)%2;
+			
+			if (Attendance == Is_Present)
+			System.out.println("Employee is Present");
+			
+			else
+				System.out.println("Employee is absent");
+		}
+
 		public static void main(String[] args) {
 			Emp_Wage_Day9 obj = new Emp_Wage_Day9();
 			obj.Welcome();
+			obj.Attendance();
 		}
 
 	}
