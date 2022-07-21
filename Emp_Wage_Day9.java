80723ce120d44297215a5493d88d809499936ba7
vishal5394 <vishaljadyal@gmail.com>
2022-07-22 00:37:35 +0530

    Added: Get Employee Monthly Wage using condition

--- Emp_Wage_Day9.java
+++ Emp_Wage_Day9.java
@@ -5,16 +5,19 @@ package com.BridgeLabz.day9;
 
 		static final int Is_Present = 1;
 		static final int Is_PartTime = 2;
-		static int Emp_wage_per_hr = 20;
-		static int No_Of_Working_Days = 20;
+		static final int Emp_wage_per_hr = 20;
+		static final int No_Of_Working_Days = 20;
+		static final int No_Of_Working_hr = 100;
 
 		void  Welcome(){
 			System.out.println("Welcome to Employee Wage Computation Program"); 
 		}
-		void getMonthlyWage() {
-			int  Employeehr = 0 , Daily_Wage = 0, Total_Wage = 0;
+		void getMonthlyWageCondition() {
+			int  Employeehr = 0 , Daily_Wage = 0, Total_Wage = 0, TotalEmployeehr =0, TotalWorkingDays =0;
 			
-			int Attendance = (int)Math.floor(Math.random()*10)%3;
+			while(TotalWorkingDays < No_Of_Working_Days && TotalEmployeehr <= No_Of_Working_hr) {
+			
+				int Attendance = (int)Math.floor(Math.random()*10)%3;
 			
 			switch (Attendance) {
 			 case Is_Present:
@@ -32,18 +35,19 @@ package com.BridgeLabz.day9;
 			System.out.println("Employee is Absent ");
 			break;
 			}
-				
+			
+			TotalEmployeehr += Employeehr;
 			Daily_Wage = Emp_wage_per_hr  *Employeehr;
 			Total_Wage = Daily_Wage * No_Of_Working_Days;
 			
 			System.out.println("Employee Daily Wage is = "+Daily_Wage);
 			System.out.println("Employee Monthly Total Wage is = "+Total_Wage);
+			}
 		}
-
 		public static void main(String[] args) {
 			Emp_Wage_Day9 obj = new Emp_Wage_Day9();
 			obj.Welcome();
-			obj.getMonthlyWage();
+			obj.getMonthlyWageCondition();
 
 		}
 	}
