e2584c41d632f50a1155f574fa8eb3e8e4277244
vishal5394 <vishaljadyal@gmail.com>
2022-07-22 00:30:56 +0530

    Added: check Employee Wage using Switch Case

--- Emp_Wage_Day9.java
+++ Emp_Wage_Day9.java
@@ -2,41 +2,45 @@ package com.BridgeLabz.day9;
 
 	public class Emp_Wage_Day9 {
 		
-	static int Is_Present = 1;
-	static int Is_PartTime = 2;
-	static int Emp_wage_per_hr = 20;
 
-	void  Welcome(){
-		System.out.println("Welcome to Employee Wage Computation Program"); 
-	}
-	void Check_Wage() {
-		int  Employeehr = 0 , Daily_Wage = 0;
-		
-		double Attendance = Math.floor(Math.random()*10)%3;
-		
-		if (Attendance == Is_Present) {
-			Employeehr = 8;
-		System.out.println("Employee is Present ");
+		static final int Is_Present = 1;
+		static final int Is_PartTime = 2;
+		static int Emp_wage_per_hr = 20;
+
+		void  Welcome(){
+			System.out.println("Welcome to Employee Wage Computation Program"); 
 		}
-		else if (Attendance == Is_PartTime) {
-			Employeehr = 4;
-		System.out.println("Employee is Present Part Time ");
-		}
-		else {
-			Employeehr = 0;
-		System.out.println("Employee is Absent ");
-		}
+		void UsingSwitch() {
+			int  Employeehr = 0 , Daily_Wage = 0;
 			
-		Daily_Wage = Emp_wage_per_hr  *Employeehr;
-		
-		System.out.println("Employee Daily Wage is = "+Daily_Wage);
-	}
+			int Attendance = (int)Math.floor(Math.random()*10)%3;
+			
+			switch (Attendance) {
+			 case Is_Present:
+				Employeehr = 8;
+					System.out.println("Employee is Present ");
+					break;
+			
+			case Is_PartTime :
+				Employeehr = 4;
+				 System.out.println("Employee is Present Part Time ");
+				 break;
+				 
+			default:
+				Employeehr = 0;
+			System.out.println("Employee is Absent ");
+			break;
+			}
+				
+			Daily_Wage = Emp_wage_per_hr  *Employeehr;
+			
+			System.out.println("Employee Daily Wage is = "+Daily_Wage);
+		}
 
-	public static void main(String[] args) {
-		Emp_Wage_Day9 obj = new Emp_Wage_Day9();
-		obj.Welcome();
-		obj.Check_Wage();
-
+		public static void main(String[] args) {
+			Emp_Wage_Day9 obj = new Emp_Wage_Day9();
+			obj.Welcome();
+			obj.UsingSwitch();
 	}
 	}
 	
