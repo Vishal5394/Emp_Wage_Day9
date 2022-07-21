705b3fce9ebb0aacd21867589d507e04162684d8
vishal5394 <vishaljadyal@gmail.com>
2022-07-22 00:27:06 +0530

    Added: check Employee Wage

--- Emp_Wage_Day9.java
+++ Emp_Wage_Day9.java
@@ -1,37 +1,42 @@
 package com.BridgeLabz.day9;
 
 	public class Emp_Wage_Day9 {
+		
+	static int Is_Present = 1;
+	static int Is_PartTime = 2;
+	static int Emp_wage_per_hr = 20;
 
-		static int Is_Present = 1;
-		static int Emp_wage_per_hr = 20;
-
-		void  Welcome(){
-			System.out.println("Welcome to Employee Wage Computation Program"); 
+	void  Welcome(){
+		System.out.println("Welcome to Employee Wage Computation Program"); 
+	}
+	void Check_Wage() {
+		int  Employeehr = 0 , Daily_Wage = 0;
+		
+		double Attendance = Math.floor(Math.random()*10)%3;
+		
+		if (Attendance == Is_Present) {
+			Employeehr = 8;
+		System.out.println("Employee is Present ");
 		}
-		void Daily_Wage() {
-			int  Employeehr = 0 , Daily_Wage = 0;
-			
-			double Attendance = Math.floor(Math.random()*10)%2;
-			
-			if (Attendance == Is_Present) {
-				Employeehr = 8;
-			System.out.println("Employee is Present ");
-			}
-			else {
-				Employeehr = 0;
-			System.out.println("Employee is Absent ");
-			}
-				
-			Daily_Wage = Emp_wage_per_hr  *Employeehr;
-			
-			System.out.println("Employee Daily Wage is = "+Daily_Wage);
+		else if (Attendance == Is_PartTime) {
+			Employeehr = 4;
+		System.out.println("Employee is Present Part Time ");
 		}
-
-		public static void main(String[] args) {
-			Emp_Wage_Day9 obj = new Emp_Wage_Day9();
-			obj.Welcome();
-			obj.Daily_Wage();
-
+		else {
+			Employeehr = 0;
+		System.out.println("Employee is Absent ");
 		}
+			
+		Daily_Wage = Emp_wage_per_hr  *Employeehr;
+		
+		System.out.println("Employee Daily Wage is = "+Daily_Wage);
+	}
+
+	public static void main(String[] args) {
+		Emp_Wage_Day9 obj = new Emp_Wage_Day9();
+		obj.Welcome();
+		obj.Check_Wage();
+
+	}
 	}
 	
