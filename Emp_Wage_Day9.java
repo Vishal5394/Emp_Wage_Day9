dc7e5229fa6ec19f7ee7f2b80b5dea637b7866c1
vishal5394 <vishaljadyal@gmail.com>
2022-07-22 00:06:27 +0530

    Added: Welcome To Employee Wage

---
+++ Emp_Wage_Day9.java
@@ -0,0 +1,14 @@
+package com.BridgeLabz.day9;
+
+	public class Emp_Wage_Day9 {
+
+		void  Welcome(){
+			System.out.println("Welcome to Employee Wage Computation Program");
+		}
+		public static void main(String[] args) {
+			Emp_Wage_Day9 obj = new Emp_Wage_Day9();
+			obj.Welcome();
+		}
+
+	}
+	
